f0ed262ea34f8e8563be488dad5c188d76c02443
yamanass <yamanassi2021@gmail.com>
2023-11-20 13:59:45 +0200

    editing and optmizing form

--- flutter_application_1/lib/screens/medicin_form.dart
+++ flutter_application_1/lib/screens/medicin_form.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/category_drop.dart';
+import 'package:flutter_application_1/widgets/date_time.dart';
+import 'package:flutter_application_1/widgets/tex_form_midicin.dart';
 
-class details extends StatefulWidget {
-  details({super.key});
+class MedicinFormScreen extends StatefulWidget {
+  MedicinFormScreen({super.key});
 
   @override
-  State<details> createState() => _detailsState();
+  State<MedicinFormScreen> createState() => _MedicinFormScreenState();
 }
 
-class _detailsState extends State<details> {
-  String? valuechoose;
-  List listItem = ["Item 1", "Item 2", "Item 3", "Item 4"];
-
+class _MedicinFormScreenState extends State<MedicinFormScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,204 +18,57 @@ class _detailsState extends State<details> {
         home: Scaffold(
           body: Padding(
             padding: const EdgeInsets.only(top: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 6),
-                  child: Text(
-                    'Medication Detail',
-                    style: TextStyle(
-                      fontSize: 35,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.deepPurple,
+            child: Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 6),
+                      child: Text(
+                        'Medication Detail',
+                        style: TextStyle(
+                          fontSize: 35,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Divider(
-                  color: Colors.grey,
-                  thickness: 2,
-                  indent: 100,
-                  endIndent: 100,
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Container(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
+                    Divider(
+                      color: Colors.grey,
+                      thickness: 2,
+                      indent: 100,
+                      endIndent: 100,
+                      height: 10,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    TextFormMidicn(labelText: "Enter the scientific name"),
+                    TextFormMidicn(labelText: "Enter the trade name"),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Row(
                         children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: BorderSide(
-                                color: Colors.deepPurple,
-                              )),
-                              labelText: "Enter the scientific name",
-                              labelStyle: TextStyle(color: Colors.deepPurple),
-                              contentPadding: EdgeInsets.symmetric(
-                                  vertical: 20, horizontal: 30),
-                              filled: true,
-                              fillColor: Colors.transparent,
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                  borderSide:
-                                      BorderSide(color: Colors.deepPurple)),
-                            ),
+                          Expanded(child: CategoryDropDown()),
+                          SizedBox(
+                            width: 16,
                           ),
-                          Container(
-                            padding: EdgeInsets.only(top: 15),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.deepPurple)),
-                                labelText: "Enter the trade name",
-                                labelStyle: TextStyle(color: Colors.deepPurple),
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 20, horizontal: 30),
-                                filled: true,
-                                fillColor: Colors.transparent,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                    borderSide: BorderSide(
-                                      color: Colors.deepPurple,
-                                    )),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 10, horizontal: 20),
-                            child: Container(
-                              padding: EdgeInsets.only(top: 15),
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Colors.grey,
-                                  width: 1,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              child: Padding(
-                                padding: EdgeInsets.only(top: 15),
-                                child: Container(
-                                  child: DropdownButton(
-                                      hint: Text(
-                                        "Select catigory",
-                                        style:
-                                            TextStyle(color: Colors.deepPurple),
-                                            
-                                      ),
-                                      dropdownColor: Colors.grey,
-                                      icon: Icon(Icons.arrow_drop_down),
-                                      isExpanded: true,
-                                      value: valuechoose,
-                                      onChanged: (newValue) {
-                                        setState(() {
-                                          valuechoose = newValue as String?;
-                                        });
-                                      },
-                                      items: listItem.map((valueItem) {
-                                        return DropdownMenuItem(
-                                          value: valueItem,
-                                          child: Text(valueItem),
-                                        );
-                                      }).toList()),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 15),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.deepPurple)),
-                                labelText: "Enter the name of the manufacturer",
-                                labelStyle: TextStyle(color: Colors.deepPurple),
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 20, horizontal: 30),
-                                filled: true,
-                                fillColor: Colors.transparent,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                    borderSide: BorderSide(
-                                      color: Colors.deepPurple,
-                                    )),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 15),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.deepPurple)),
-                                labelText: "Enter the available quantity",
-                                labelStyle: TextStyle(color: Colors.deepPurple),
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 20, horizontal: 30),
-                                filled: true,
-                                fillColor: Colors.transparent,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                    borderSide: BorderSide(
-                                      color: Colors.deepPurple,
-                                    )),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 15),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.deepPurple)),
-                                labelText: " Enter date entihaa salahia",
-                                labelStyle: TextStyle(color: Colors.deepPurple),
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 20, horizontal: 30),
-                                filled: true,
-                                fillColor: Colors.transparent,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                    borderSide: BorderSide(
-                                      color: Colors.deepPurple,
-                                    )),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 15),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                focusedBorder: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.deepPurple)),
-                                labelText: "Enter the price",
-                                labelStyle: TextStyle(color: Colors.deepPurple),
-                                contentPadding: EdgeInsets.symmetric(
-                                    vertical: 20, horizontal: 30),
-                                filled: true,
-                                fillColor: Colors.transparent,
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                    borderSide: BorderSide(
-                                      color: Colors.deepPurple,
-                                    )),
-                              ),
-                            ),
-                          ),
+                          Expanded(child: PickDateWidget())
                         ],
                       ),
                     ),
-                  ),
+                    TextFormMidicn(labelText: "Enter the manufacturer name"),
+                    TextFormMidicn(labelText: "Enter the available quantity"),
+                    TextFormMidicn(labelText: " Enter date entihaa salahia"),
+                    TextFormMidicn(labelText: "Enter the price"),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ));
